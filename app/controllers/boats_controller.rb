b40d9bff22baa3bcb0558002b5b61bdd21b52a0b
John <jjh5166@gmail.com>
2018-03-02 12:57:32 -0500

    boat controller edits

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,14 +1,17 @@
 class BoatsController < ApplicationController
   def create
-    @boat = Boat.create(user_params)
+    @boat = Boat.create(boat_params)
   end
-
+  def show
+    @boat = Boat.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
   private
 
   # Use strong_parameters for attribute whitelisting
   # Be sure to update your create() and update() controller methods.
 
-  def user_params
+  def boat_params
     params.require(:user).permit(:avatar)
   end
 end
