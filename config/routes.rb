0cbe09ae9811393db8f6fb7a933dcd7fa51ec461
Aaron Brown <ayayron44@gmail.com>
2018-03-02 12:17:07 -0500

    added sign out link to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'devise/sessions'
   }
   devise_scope :user do
     authenticated :user do
