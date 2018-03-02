158ada558bc00b5fe0c1d189bc820502b65a5f85
YOUR NAME <rodrigocortes98@live.com>
2018-03-02 12:24:54 -0500

    added page routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   devise_scope :user do
     authenticated :user do
       root 'users#index', as: :authenticated_root
+      resources :boats
     end
 
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
+  resources :jobs
 end
