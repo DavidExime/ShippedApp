a113c57a899a5681937a1534a713f6b3cc554c1b
Aaron Brown <ayayron44@gmail.com>
2018-03-02 16:48:09 -0500

    created boatForm partial and added flash message to layout

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,6 @@
 class Boat < ApplicationRecord
+  validates :capacity, numericality: {greater_than: 0}
+  validates :capacity, numericality: {only_integer: true}
   has_and_belongs_to_many :jobs
   belongs_to :user
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
