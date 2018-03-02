4be58a2c1421e61a939282c78a154aa513918d84
Aaron Brown <ayayron44@gmail.com>
2018-03-02 11:13:20 -0500

    created users controller and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   devise_scope :user do
     authenticated :user do
-      root 'home#index', as: :authenticated_root
+      root 'users#index', as: :authenticated_root
     end
 
     unauthenticated do
