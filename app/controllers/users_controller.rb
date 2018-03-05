977d9c8fc305f38990c04fd3a3bb5c5718956c3f
Aaron Brown <ayayron44@gmail.com>
2018-03-05 09:51:37 -0500

    users profile and users list added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @boats = Boat.all
+    @users = User.all
+    @user = User.find_by_email(params[:id])
     @current_user = current_user.id
   end
 
@@ -14,6 +15,8 @@ class UsersController < ApplicationController
 
   def show
     @current_user = current_user.id
+    @user = User.find(params[:id])
+    @boats = Boat.all
   end
 
   def edit
