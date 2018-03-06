533a96446e943da172e92c656ea072483f6ca486
YOUR NAME <rodrigocortes98@live.com>
2018-03-06 09:49:18 -0500

    fixed issue

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,29 +17,12 @@ class JobsController < ApplicationController
   def create
 
   	@job = Job.new(job_params)
-  	@job.recontainers = @job.totalcontainers  
-  	@job.cost = @job.totalcontainers * 100  
-  	# if(@job.cost.to_f < 100)
-   #        flash[:message] = 'All Jobs Must Cost Min $100'
-   #          redirect_to jobs_path     
-   #  end
-   #  if(@job.description.length < 20)
-   #        flash[:message] = 'Descriptions must be longer than 20 characters'
-   #          redirect_to jobs_path 
-   #  end
+  	@job.recontainers = @job.totalcontainers
+  	@job.cost = @job.totalcontainers * 100
   	if @job.save
-
   		redirect_to "/jobs"
   	else
   		redirect_to new_job_path
-
-  		flash[:message] = 'Job saved'
-  		redirect_to jobs_path
-
-  	else      
-  		render "/jobs/new"
-  		flash[:message] = 'Job unsaved'
-
   	end
   end
 
@@ -73,18 +56,12 @@ class JobsController < ApplicationController
     @current_user = current_user.id
   end
   def assignjob
-    boat = Boat.find(params[:id])
-    job = Job.find(params[:jobid])
-    params[:containers] = params[:containers].to_i
-    boat.jobs << job
-    work = Work.where(job_id: params[:jobid], boat_id: params[:id])
-    work.update(containers: params[:containers])
-    boat.increment!('loadtaken', params[:containers])
-    job.increment!('recontainers', -params[:containers])
+    @boat = Boat.find(params[:id])
+    @job = Job.find(params[:jobid])
+    @boat.jobs << @job
     redirect_to user_boat_path(id: params[:id], user_id: current_user.id)
   end
   private
-
 
   def job_params
     params.require(:job).permit(:description, :origin, :destination, :cost, :totalcontainers, :recontainers)
