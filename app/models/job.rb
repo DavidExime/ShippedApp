3232783117f87665b700724e842f249b0dc3868d
John <jjh5166@gmail.com>
2018-03-05 20:53:04 -0500

    fixed job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,16 +1,6 @@
 class Job < ApplicationRecord
-<<<<<<< HEAD
-  has_and_belongs_to_many :boats
 validates :origin, :presence => true
 validates :destination, :presence => true
-
-
-
-=======
-  has_many :works
-  has_many :boats, :through => :works
->>>>>>> master
+has_many :works
+has_many :boats, :through => :works
 end
-
-
-# sets rules for the user 
