bb24a820990a21691c4767b50510bbfbe7e8cf18
John <jjh5166@gmail.com>
2018-03-06 10:56:08 -0500

    jobs controller fixed

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -56,9 +56,14 @@ class JobsController < ApplicationController
     @current_user = current_user.id
   end
   def assignjob
-    @boat = Boat.find(params[:id])
-    @job = Job.find(params[:jobid])
-    @boat.jobs << @job
+    boat = Boat.find(params[:id])
+    job = Job.find(params[:jobid])
+    params[:containers] = params[:containers].to_i
+    boat.jobs << job
+    work = Work.where(job_id: params[:jobid], boat_id: params[:id])
+    work.update(containers: params[:containers])
+    boat.increment!('loadtaken', params[:containers])
+    job.increment!('recontainers', -params[:containers])
     redirect_to user_boat_path(id: params[:id], user_id: current_user.id)
   end
   private
