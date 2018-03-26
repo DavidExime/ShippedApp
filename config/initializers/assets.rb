b1bea252aff412758426b52a48585becd1427608
YOUR NAME <rodrigocortes98@live.com>
2018-03-26 12:49:32 -0400

    styled the jobs list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
-add this to config/initializer/assets.rb file
+# config/initializer/assets.rb
